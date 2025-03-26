4dd871636573adb6a8dcf020e540e309423da2dc
João Lima <joaoazevedolima@gmail.com>
2025-03-26 14:33:15 +0000

    ST-2372: Bump version, update ska-base-images and updated makefile

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -24,9 +24,9 @@ copyright = '2025 SKAO'
 author = 'System Team'
 
 # The short X.Y version
-version = '0.0.3'
+version = '0.0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
